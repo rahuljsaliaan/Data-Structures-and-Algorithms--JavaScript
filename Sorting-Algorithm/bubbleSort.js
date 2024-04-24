a5e3920c9cce2b49a9bca79d9cf79286bafc70fc
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-04-24 21:32:02 +0530

    refactor: optimized bubble sort to not sort if already sorted

--- Sorting-Algorithm/bubbleSort.js
+++ Sorting-Algorithm/bubbleSort.js
@@ -2,6 +2,8 @@ function swap(arr, index) {
   let temp = arr[index];
   arr[index] = arr[index + 1];
   arr[index + 1] = temp;
+
+  return true;
 }
 
 // Version 1
@@ -15,13 +17,28 @@ function swap(arr, index) {
 //   }
 // }
 
+// version2
+// function bubbleSort(numArr) {
+//   for (let i = numArr.length - 1; i > 0; i--) {
+//     for (let j = 0; j < i; j++) {
+//       if (numArr[j] > numArr[j + 1]) {
+//         swap(numArr, j);
+//       }
+//     }
+//   }
+// }
+
+// Optimized version
+// EXPL: Do not sort if already sorted (is there is no swap performed in the inner loop)
 function bubbleSort(numArr) {
   for (let i = numArr.length - 1; i > 0; i--) {
+    let isSwap = false;
     for (let j = 0; j < i; j++) {
       if (numArr[j] > numArr[j + 1]) {
-        swap(numArr, j);
+        isSwap = swap(numArr, j);
       }
     }
+    if (!isSwap) break;
   }
 }
 
