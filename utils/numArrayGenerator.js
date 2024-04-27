ddc1837ee9eeedeb8cd9672d51f478f3a74fafcc
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-04-27 19:27:04 +0530

    refactor: negative number array generator to also generate negative numbers

--- utils/numArrayGenerator.js
+++ utils/numArrayGenerator.js
@@ -1,5 +1,8 @@
 function numArrayGenerator(size) {
-  return Array.from({ length: size }, () => Math.floor(Math.random() * size));
+  return Array.from(
+    { length: size },
+    () => Math.floor(Math.random() * size * 2) - size
+  );
 }
 
 module.exports = numArrayGenerator;
